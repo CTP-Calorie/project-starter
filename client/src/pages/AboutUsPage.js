0f48bd85d46536a8300f6e5e42761e10c542de08
Javier M <73079627+JavierMajano@users.noreply.github.com>
2021-12-08 22:55:50 -0500

    Added bg color to about us page

--- client/src/pages/AboutUsPage.js
+++ client/src/pages/AboutUsPage.js
@@ -1,77 +1,32 @@
 import React from 'react';
+import './about.css'
 
 function AboutUsPage(props) {
   return (
     <div>
   
 
-    <div id="myCarousel" className="carousel slide" data-ride="carousel">
-      <ol className="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1" className=""></li>
-        <li data-target="#myCarousel" data-slide-to="2" className=""></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img className="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide" />
-          <div className="container">
-            <div className="carousel-caption text-left">
-              <h1>Example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img className="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide" />
-          <div className="container">
-            <div className="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a className="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img className="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide" />
-          <div className="container">
-            <div className="carousel-caption text-right">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a className="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-
+   
+<div className='bg-info'>
     <div className="container marketing">
 
       <div className="row">
         <div className="col-lg-4">
           <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder " width="140" height="140" />
-          <h2>Seth Marcus</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          <h2 className='information'> Seth Marcus</h2>
+          <p className='about'>Donec sed odio dui.  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p ><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
         <div className="col-lg-4">
           <img className="rounded-circle" src="../images/Javier.jpg" alt="Generic placeholder " width="150" height="150"/>
-          <h2>Javier Majano</h2>
-          <p>CUNY John Jay Student</p>
+          <h2 className='information'>Javier Majano</h2>
+          <p className='about'>CUNY John Jay Student</p>
           <p><a className="btn btn-secondary" href="https://infinite-anchorage-16926.herokuapp.com/"   target="_blank" rel="noopener noreferrer"role="button">My website »</a></p>
         </div>
         <div className="col-lg-4">
           <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder " width="140" height="140"/>
-          <h2>Evan Adis</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <h2 className='information'>Evan Adis</h2>
+          <p className='about'>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. VFusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
       </div>
@@ -121,11 +76,12 @@ function AboutUsPage(props) {
 
     </div>
 
-
+    </div>
     <footer className="container">
       <p className="float-right"><a href="#">Back to top</a></p>
-      <p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+      <p>© 2021 Caloric Conscious Clan, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
     </footer>
+
  </div>
   );
 }
